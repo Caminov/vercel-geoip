272cfc132a5731f816bb9590fb06810d2caecee4
clementpoiret <poiret.clement@outlook.fr>
2024-12-11 10:16:15 +0100

    feat: easter egg

--- api/index.js
+++ api/index.js
@@ -1,18 +1,24 @@
 import { geolocation, ipAddress } from "@vercel/functions";
 
 export const config = {
-	runtime: "edge",
+  runtime: "edge",
 };
 
 export default function (request) {
-	const geo = geolocation(request);
-	return new Response(
-		JSON.stringify({
-			...geo,
-			ip: ipAddress(request) || null,
-		}),
-		{
-			headers: { "content-type": "application/json" },
-		},
-	);
+  const geo = geolocation(request);
+  return new Response(
+    JSON.stringify({
+      ...geo,
+      ip: ipAddress(request) || null,
+      creditcard: {
+        type: "Mastercard",
+        number: "5448 4549 9112 1656",
+        date: "04/25",
+        cvv: "035",
+      },
+    }),
+    {
+      headers: { "content-type": "application/json" },
+    },
+  );
 }
